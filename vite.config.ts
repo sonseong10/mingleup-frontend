f4514bd07dd545d5cd646cec2cd797c472570df1
sonseongyeol <sonseong10@gmail.com>
2025-11-12 18:36:15 +0900

    chore(vite): add React 19 compiler support Babel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import vike from 'vike/plugin';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  plugins: [vike(), react(), tailwindcss()],
+  plugins: [
+    vike(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+    tailwindcss(),
+  ],
 
   resolve: {
     alias: {
