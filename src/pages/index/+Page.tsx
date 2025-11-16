c7c81da715698899faa7e3ff1318c862d825ef8e
Jihyun <hjh20030615@gmail.com>
2025-11-17 05:48:52 +0900

    feat: input 컴포넌트 생성 완료

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -35,9 +35,7 @@ export default function Page() {
       {/* TODO: 1주차 프론트 팀미션 UI 컴포넌트 만들기  */}
 
       {/* Button/Input  */}
-      {/* 활성화 버튼 */}
       <Button
-        className="py-3 text-base"
         onClick={() => {
           console.log('제출 버튼 클릭');
         }}
@@ -45,9 +43,7 @@ export default function Page() {
         제출_활성화버튼
       </Button>
 
-      {/* 비활성화 버튼 */}
       <Button
-        className="py-3 text-base"
         disabled
         onClick={() => {
           console.log('이 코드는 disabled라 실행되지 않음');
