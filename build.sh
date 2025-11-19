bb87c30aaf7807c2941751f2bccde12492ed1e60
sonseongyeol <sonseong10@gmail.com>
2025-11-19 13:22:30 +0900

    build: deploy git actions setting

--- build.sh
+++ build.sh
@@ -1,15 +1,4 @@
-#!/bin/sh
-set -e
-
-# build
-npm install
-npm run build
-
-rm -rf output
+cd ../
 mkdir output
-
-# dist 전체 복사
-cp -R dist/* output
-
-# vercel.json도 output으로 복사
-cp vercel.json output
+cp -R ./mingleup-frontend/* ./output
+cp -R ./output ./mingleup-frontend/
